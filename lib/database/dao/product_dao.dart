6bb704e11e98e6aa9ea09247ffaa6acf9da477b2
waxew <4622.ar@gmail.com>
2026-09-02 17:10:50 +0330

    feat(database): connect product dao to sqlite

--- lib/database/dao/product_dao.dart
+++ lib/database/dao/product_dao.dart
@@ -1,20 +1,29 @@
 import '../frooshyar_repository.dart';
+import '../sqlite/frooshyar_sqlite_helper.dart';
 
-/// Product and inventory data access layer.
+/// Product and inventory CRUD operations.
 class ProductDao {
   final FrooshyarRepository repository;
 
   ProductDao(this.repository);
 
   Future<List<Map<String, dynamic>>> getAll() async {
-    return [];
+    final db = await FrooshyarSqliteHelper.instance.database;
+    return db.query('products', orderBy: 'id DESC');
   }
 
   Future<int> insert(Map<String, dynamic> product) async {
-    return 0;
+    final db = await FrooshyarSqliteHelper.instance.database;
+    return db.insert('products', product);
   }
 
-  Future<void> update(Map<String, dynamic> product) async {}
+  Future<int> update(Map<String, dynamic> product) async {
+    final db = await FrooshyarSqliteHelper.instance.database;
+    return db.update('products', product, where: 'id = ?', whereArgs: [product['id']]);
+  }
 
-  Future<void> delete(int id) async {}
+  Future<int> delete(int id) async {
+    final db = await FrooshyarSqliteHelper.instance.database;
+    return db.delete('products', where: 'id = ?', whereArgs: [id]);
+  }
 }
